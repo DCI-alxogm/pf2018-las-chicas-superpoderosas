82f41fe2035733792be2ae1e6f76a9264731cb0d
elimer <liz.eli_@hotmail.com>
2018-11-28 09:39:03 -0600

    funcion main

---
+++ main.c
@@ -0,0 +1,11 @@
+/*Funcion main del proyecto final de programación, proyecto ncuerpos simulación de gases*/
+ 
+#include<stdio.h>
+#include<stdilib.h>
+
+void fuerza();
+
+int main(){
+	fuerza();
+	return 0;
+}
