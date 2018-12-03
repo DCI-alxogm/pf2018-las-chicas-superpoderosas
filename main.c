2bf9d6c8d22df06831b8c6fd79d446f142f3ec4f
Cruz Aguilar Montserrat <cruzam2018@dci25-164.ugto.mx>
2018-12-03 09:37:18 -0600

    .

---
+++ main.c
@@ -0,0 +1,12 @@
+/*Funcion main del proyecto final de programación, proyecto n cuerpos simulación de gases*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+int proyecto3(){
+	proyecto3();
+	return 0;
+}
