4a79147f28c35030d1ba8da1bbeac28ea020150d
Mercado Ruiz Elizabeth <mercadore2018@dci25-190.ugto.mx>
2018-12-04 11:24:50 -0600

    avance

--- void.c
+++ void.c
@@ -5,7 +5,6 @@
 #include<math.h>
 
 void proyecto3(){
-	float pi=3.1416;
 	float masas,d,e=1,fuerza,rho;//d es la distancia entre dos puntos y es un puntero ya que es un valor diferente para cada partícula
 	float G=4*(pow(pi,2));
         FILE *datos;
@@ -23,3 +22,5 @@ pow(d[],7))*j
 fuerzaz[]=4*e((12*(pow(rho,12))/(pow(d[],13))-((6*(pow(rho,6)/(
 pow(d[],7))*k
 }
+
+
