003c76b495c1db3ea2fe34a8719b6a4f742e276b
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-163.ugto.mx>
2018-12-07 15:38:41 -0600

    malla

--- malla.c
+++ malla.c
@@ -2,29 +2,35 @@
 #include<stdlib.h>
 
 
-int main(){ //malla de masas y posiciones iniciales
+int main(){ 
+//Malla para las masas iniciales y posiciones
 
+        //Variables
 	float h,m,G;
 	int i,x0,y0,z0,n=0;
-	int *x= (int*)malloc(n*sizeof(float));
-	int *y= (int*)malloc(n*sizeof(float));
-	int *z= (int*)malloc(n*sizeof(float));
-	
-
+	int *x = (int*)malloc(n*sizeof(float));
+        
+	int *y = (int*)malloc(n*sizeof(float));
+        
+	int *z = (int*)malloc(n*sizeof(float));
+        
+        
 	FILE *in;
 	FILE *datos;
 
-
-	datos=fopen("datosprueba.txt","r");
-	fscanf("%i %f %i %i %i %f",&n,&m,&x0,&y0,&z0,&h,&G);
-	fclose(datos);
+        //abro mi archivo donde se encuentran los valores de las variables y lo escaneo
+	datos=fopen("datos.txt","r");
+        fscanf(datos,"%i %f %i %i %i %f %f",&n,&m,&x0,&y0,&z0,&h,&G);
+        fclose(datos);
+        
 	
-	x[0]=x0;
+	
+        //abro un archivo de tipo write en el cual se encontraran los resultados de la malla
+	in=fopen("malla.txt","w");
+        
+        x[0]=x0;
 	y[0]=y0;
-	z[0]=z0;
-	
-	in=fopen("malla3.txt","w");
-        fclose(in);
+        z[0]=z0;
 
 	for(i=0;i<n;i++){
 		x[i]=x[0];
@@ -32,6 +38,7 @@ int main(){ //malla de masas y posiciones iniciales
 		z[i]=z[0];
 		fprintf(in,"%d \t x: %i, \t y: %i, \t  z: %i, \t \n",i,x[i],y[i],z[i]);
 	}
+        fclose(in);
 		
 	free (x);
 	free (y);
