75a2e666f2f580243e46730be377e6af8560d229
Cruz Aguilar Montserrat <cruzam2018@dci25-164.ugto.mx>
2018-12-07 15:55:45 -0600

    Ac

--- void.c
+++ void.c
@@ -6,8 +6,50 @@
 
 void proyecto3(){ //Se la da inicio al programa, pero en lugar de usar la funcion main, utilizamos el nombre del primer archivo
 
-
 //Declaración de variables
+		
+	float rx[],ry[],rz[],mp=0.1,
+	int m,G=43000                                                                                                                                                                                                                                                                                                                                                                                                                                                      ,i,j;
+	double x[n],y[n],z[n];
+
+
+FILE *datos;
+FILE *resultados;
+//Se tomara cada planeta en cuestion en determinado tiempo, para que en base a la relación de la fuerza, velocidades y también. 
+
+for (j=0;j<n;j++)
+{
+    for(i=0;i<n;i++)
+	{
+if 
+
+	 //Hacemos las operaciones pertinentes para relación entre planetas con cada uno de los ejes
+	fx[j]= ((G*m*m)) / pow(fabs(rx[i]-rx[i+1]),3) *fabs (rx[i]-rx[i+1]) +fx[j]; //trabajando con el eje x
+	
+	fy[j]= ((G*m*m)) / pow(fabs(ry[i]-ry[i+1]),3) *fabs (ry[i]-ry[i+1]) +fy[j]; //trabajando con el eje y
+
+	fz[j]= ((G*m*m)) / pow(fabs(rz[i]-rz[i+1]),3) *fabs (rz[i]-rz[i+1]) +fz[j];  //trabajando con el eje z
+
+	//Se imprimen los resultados en otro archivo de texto.
+	fprintf(resultados,"%lf\t %lf\t %lf\t",x[i],y[i],z[i],h);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	float pi=3.1416;
 	float masas,d,e=1,fuerza,rho;//d es la distancia entre dos puntos y es un puntero ya que es un valor diferente para cada partícula
 	float G=4*(pow(pi,2));
@@ -28,5 +70,4 @@ pow(d[],7))*j
 fuerzaz[]=4*e((12*(pow(rho,12))/(pow(d[],13))-((6*(pow(rho,6)/(
 pow(d[],7))*k
 }
-
 
