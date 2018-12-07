0276a677968448879d4af6a7f632c6a4508cc0f7
Mercado Ruiz Elizabeth <mercadore2018@dci25-164.ugto.mx>
2018-12-07 12:28:08 -0600

    .

--- malla.c
+++ malla.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
-void malla(){
+
+
+int main(){ //malla de masas y posiciones iniciales
+
 	float h,m,G;
-	int i,xi,yi,zi,n=0;
+	int i,x0,y0,z0,n=0;
 	int *x= (int*)malloc(n*sizeof(float));
 	int *y= (int*)malloc(n*sizeof(float));
 	int *z= (int*)malloc(n*sizeof(float));
 	
 
 	FILE *in;
-	FILE *datos
+	FILE *datos;
 
 
 	datos=fopen("datosprueba.txt","r");
-	fscanf("%i %f %i %i %i %f",&n,&m,&x[0],&y[0],&z[0],&h,&G);
+	fscanf("%i %f %i %i %i %f",&n,&m,&x0,&y0,&z0,&h,&G);
 	
 	fclose(datos);
-
+	
+	x[0]=x0;
+	y[0]=y0;
+	z[0]=z0;
+	
 	in=fopen("malla3.txt","w");
 
 	for(i=0;i<n;i++){
@@ -31,5 +38,6 @@ void malla(){
 	free (z);
 return 0;
 }
+
 
 
