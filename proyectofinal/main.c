b2088d237566ed4931a356a2be1a49bc5a0cddb9
root <root@LAPTOP-F9GTBTKU.localdomain>
2018-12-09 20:40:41 -0600

    El usuario que dice root es elimer

--- proyectofinal/main.c
+++ proyectofinal/main.c
@@ -1,12 +1,12 @@
 /*Funcion main del proyecto final de programación, proyecto n cuerpos*/
 
 #include<stdio.h>
-#include "main.h"
+#include "main.h "//se añade esta libreria que es la que contiene las funciones utilizadas en todo el proyecto
 
 int main(){ //Se le da inicio al programa
 
-	proyecto();
+	proyecto(); //se llama a la funcion proyecto
 
 
 	return 0;
-}
+}//se cierra el programa 
